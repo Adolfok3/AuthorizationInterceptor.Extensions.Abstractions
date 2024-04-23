6d46e0a3126acde9f4187558401198482d035041
Luiz Adolfo <luiz-adolfo@hotmail.com>
2024-04-23 18:18:15 -0300

    initial-impl

---
+++ src/Headers/AuthorizationHeaders.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AuthorizationInterceptor.Extensions.Abstractions.Headers
+{
+    /// <summary>
+    /// Represents the headers returned by a authorization interceptor process or authentication method and including expiration information.
+    /// </summary>
+    public class AuthorizationHeaders : Dictionary<string, string>
+    {
+        /// <summary>
+        /// Obtém as propriedades do OAuth depois de definidas no método de autenticação.
+        /// </summary>
+        public OAuthHeaders? OAuthHeaders { get; private set; }
+
+        /// <summary>
+        /// Gets the optional expiration timespan for the authorization data.
+        /// </summary>
+        /// <value>
+        /// The duration for which the authorization data is considered valid, or <c>null</c> if it does not expire.
+        /// </value>
+        public TimeSpan? ExpiresIn { get; private set; }
+
+        /// <summary>
+        /// Gets the time at which the authorization data was generated/authenticated.
+        /// </summary>
+        /// <value>
+        /// The <see cref="DateTimeOffset"/> representing the point in time the authorization data was generated/authenticated.
+        /// </value>
+        public DateTimeOffset AuthenticatedAt { get; private set; }
+
+        /// <summary>
+        /// Calculate the real expiration time
+        /// </summary>
+        /// <returns>Returns the real expiration time relative to now</returns>
+        public TimeSpan? GetRealExpiration()
+            => ExpiresIn - (DateTimeOffset.UtcNow - AuthenticatedAt);
+
+        public AuthorizationHeaders(TimeSpan? expiresIn = null)
+        {
+            ExpiresIn = expiresIn;
+            AuthenticatedAt = DateTimeOffset.UtcNow;
+        }
+
+        private AuthorizationHeaders(OAuthHeaders oAuthHeaders)
+        {
+            AuthenticatedAt = DateTimeOffset.UtcNow;
+            OAuthHeaders = oAuthHeaders;
+            ExpiresIn = oAuthHeaders.ExpiresIn.HasValue ? TimeSpan.FromSeconds(oAuthHeaders.ExpiresIn.Value) : null;
+            Add("Authorization", $"{oAuthHeaders.TokenType} {oAuthHeaders.AccessToken}");
+        }
+
+        private AuthorizationHeaders()
+        {
+        }
+
+        public static implicit operator AuthorizationHeaders(OAuthHeaders headers)
+        {
+            ValidateValue(nameof(headers.AccessToken), headers.AccessToken);
+            ValidateValue(nameof(headers.TokenType), headers.TokenType);
+            if (headers.ExpiresIn.HasValue && headers.ExpiresIn.Value <= 0)
+                throw new ArgumentException("ExpiresIn must be greater tha 0.");
+
+            return new AuthorizationHeaders(headers);
+        }
+
+        private static void ValidateValue(string name, string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                throw new ArgumentException($"Property '{name}' is required.");
+        }
+    }
+}
---
+++ tests/AuthorizationInterceptor.Extensions.Abstractions.Tests/Headers/AuthorizationHeadersTests.cs
@@ -0,0 +1,88 @@
+﻿using AuthorizationInterceptor.Extensions.Abstractions.Headers;
+
+namespace AuthorizationInterceptor.Extensions.Abstractions.Tests.Headers;
+
+public class AuthorizationHeadersTests
+{
+    [Fact]
+    public void CreateAuthorizationHeaders_WithDefaultProperties_ShouldContainsCorrectInformation()
+    {
+        //Arrange
+        var headers = new AuthorizationHeaders();
+
+        //Assert
+        Assert.Null(headers.ExpiresIn);
+        Assert.Null(headers.OAuthHeaders);
+        Assert.Empty(headers);
+        Assert.NotEqual(DateTimeOffset.MinValue, headers.AuthenticatedAt);
+    }
+
+    [Fact]
+    public void CreateAuthorizationHeaders_WithProperties_ShouldContainsCorrectInformation()
+    {
+        //Arrange
+        var headers = new AuthorizationHeaders(TimeSpan.FromMinutes(3))
+        {
+            { "Test", "Test" }
+        };
+
+        //Assert
+        Assert.NotNull(headers.ExpiresIn);
+        Assert.NotEmpty(headers);
+        Assert.NotEqual(headers.ExpiresIn, headers.GetRealExpiration());
+        Assert.Contains(headers, a => a.Key == "Test" && a.Value == "Test");
+        Assert.NotEqual(DateTimeOffset.MinValue, headers.AuthenticatedAt);
+    }
+
+    [Fact]
+    public void CreateAuthorizationHeaders_FromOAuth_WithCorrectProperties_ShouldCreateCorrectly()
+    {
+        //Arrange
+        AuthorizationHeaders headers = new OAuthHeaders("Token", "Bearer", 3000, "refreskToken", "client_credentials");
+
+        //Assert
+        Assert.NotNull(headers.ExpiresIn);
+        Assert.Equal(TimeSpan.FromSeconds(3000), headers.ExpiresIn);
+        Assert.NotEmpty(headers);
+        Assert.Contains(headers, a => a.Key == "Authorization" && a.Value == "Bearer Token");
+        Assert.NotEqual(DateTimeOffset.MinValue, headers.AuthenticatedAt);
+        Assert.NotNull(headers.OAuthHeaders);
+        Assert.Equal("Token", headers.OAuthHeaders.AccessToken);
+        Assert.Equal("Bearer", headers.OAuthHeaders.TokenType);
+        Assert.Equal(3000, headers.OAuthHeaders.ExpiresIn);
+        Assert.Equal("refreskToken", headers.OAuthHeaders.RefreshToken);
+        Assert.Equal("client_credentials", headers.OAuthHeaders.Scope);
+    }
+
+    [Fact]
+    public void CreateAuthorizationHeaders_FromOAuth_WithCustomProperties_ShouldCreateCorrectly()
+    {
+        //Arrange
+        AuthorizationHeaders headers = new OAuthHeaders("Token", "Bearer");
+
+        //Assert
+        Assert.Null(headers.ExpiresIn);
+        Assert.NotEmpty(headers);
+        Assert.Contains(headers, a => a.Key == "Authorization" && a.Value == "Bearer Token");
+        Assert.NotEqual(DateTimeOffset.MinValue, headers.AuthenticatedAt);
+    }
+
+    [Fact]
+    public void CreateAuthorizationHeaders_FromOAuth_WithWrongProperties_ShouldNotCreate()
+    {
+        //Arrange
+        var entryWithToken = new OAuthHeaders("test", "");
+        var entryWithType = new OAuthHeaders("", "test");
+        var entryExpires = new OAuthHeaders("test", "test", -30000);
+
+        //Act
+        var act1 = () => (AuthorizationHeaders)entryWithToken;
+        var act2 = () => (AuthorizationHeaders)entryWithType;
+        var act3 = () => (AuthorizationHeaders)entryExpires;
+
+        //Assert
+        Assert.Equal("Property 'TokenType' is required.", Assert.Throws<ArgumentException>(act1).Message);
+        Assert.Equal("Property 'AccessToken' is required.", Assert.Throws<ArgumentException>(act2).Message);
+        Assert.Equal("ExpiresIn must be greater tha 0.", Assert.Throws<ArgumentException>(act3).Message);
+    }
+}
