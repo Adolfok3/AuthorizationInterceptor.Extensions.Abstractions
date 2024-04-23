872f76ca3a5e7ce42cc543892f04f63a5a8ea8bb
Luiz Adolfo <luiz-adolfo@hotmail.com>
2024-04-23 19:54:33 -0300

    fixed authentication handler response type

--- src/Handlers/IAuthenticationHandler.cs
+++ src/Handlers/IAuthenticationHandler.cs
@@ -11,14 +11,14 @@ namespace AuthorizationInterceptor.Extensions.Abstractions.Handlers
         /// <summary>
         /// Implementation of the authentication method.
         /// </summary>
-        /// <returns><see cref="AuthorizationHeaders"></returns>
-        Task<AuthorizationHeaders> AuthenticateAsync();
+        /// <returns><see cref="AuthorizationHeaders?"></returns>
+        Task<AuthorizationHeaders?> AuthenticateAsync();
 
         /// <summary>
         /// Implementation of the non-authentication method called when authorization is expired. Most used for refresh token actions. If the integration does not have the refresh token option, the same value as the Authenticate method must be returned.
         /// </summary>
-        /// <param name="headers">Authorization headers previously provided in the authentication method</param>
-        /// <returns><see cref="AuthorizationHeaders"></returns>
-        Task<AuthorizationHeaders> UnauthenticateAsync(AuthorizationHeaders? headers);
+        /// <param name="expiredHeaders">Expired authorization headers previously provided in the authentication method</param>
+        /// <returns><see cref="AuthorizationHeaders?"></returns>
+        Task<AuthorizationHeaders?> UnauthenticateAsync(AuthorizationHeaders? expiredHeaders);
     }
 }
